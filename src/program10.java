1b79b33d77080fd3fe3cffa61e6ca0aea3370d15
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-28 17:29:28 +0530

    third

---
+++ src/program10.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class program10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{	
+			Scanner in=new Scanner(System.in);
+			char c=in.next().charAt(0);
+			if(Character.isUpperCase(c))
+			{
+				System.out.println(c+"->"+Character.toLowerCase(c));
+			}
+			else
+			{
+				System.out.println(c+"->"+Character.toUpperCase(c));
+			}
+		}
+	}
+
+}
---
+++ src/program14.java
@@ -0,0 +1,16 @@
+
+public class program14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int i;
+			for(i=23;i<=57;i++){
+				if(i%2==0){
+				System.out.println(i);
+				}
+			}
+		}
+	}
+
+}
