f7336c8c39578178a65c2d6f515fe0d2308eb15c
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-28 17:39:55 +0530

    fourth

---
+++ src/program18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class program18{
+	private static Scanner sc;
+	public static void main(String[] args){
+		long sum=0;
+		long num; 
+		sc=new Scanner(System.in);
+		System.out.println("enter number :");
+		num=sc.nextLong();
+		for(;num!=0;){
+			sum=sum+(num%10);
+			num=num/10;
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ src/program20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class program20 {
+	private static Scanner sc;
+	public static void main(String[] args){
+		sc=new Scanner(System.in);
+		long num,rev=0;
+		System.out.println("enter number :");
+		num=sc.nextLong();
+		long k=num;
+		while(num!=0) {
+			rev=rev*10+(num%10);
+			num=num/10;
+		}
+		System.out.println("the reverse of "+k+" is :"+rev);
+		}
+
+}
